06210c877d06d41273797fefd2fbc0728e2b1c7c
artemkha <khaapt@gmail.com>
2016-11-16 22:37:46 +0700

    add page+style+file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,24 +5,25 @@ class BooksController < ApplicationController
 
 	def index
 		if params[:category].blank?
-			@books = Book.all.order("created_at DESC")
+			@books = Book.all.order("created_at DESC").page params[:page]
 		else
 			@category_id = Category.find_by(name: params[:category]).id
-			@books = Book.where(:category_id => @category_id).order("created_at DESC")
+			@books = Book.where(:category_id => @category_id).order("created_at DESC").page params[:page]
+		end
+	end
+
+		def show
+		#for average_rating of book
+		if @book.reviews.blank?
+			@average_review = 0
+		else
+			@average_review = @book.reviews.average(:rating).round(2)
 		end
 	end
 
 	def new
 		@book = current_user.books.build
 		@categories = Category.all.map{ |c| [c.name, c.id] }
-	end
-
-	def show
-		if @book.reviews.blank?
-			@average_review = 3
-		else
-			@average_review = @book.reviews.average(:rating).round(2)
-		end
 	end
 
 	def create
@@ -32,7 +33,8 @@ class BooksController < ApplicationController
 		if @book.save
 			redirect_to root_path
 		else
-			render 'new'
+			render 'new' #- there are some errors
+			#redirect_to new_book_path
 		end
 	end
 
@@ -41,11 +43,12 @@ class BooksController < ApplicationController
 	end
 
 	def update
-		@book.category_id = params[:category_id]
+		@book.category_id = params[:category_id]		
 		if @book.update(book_params)
-			redirect_to root_path
+			redirect_to root_path(@book)
 		else
-			render 'edit'
+			render 'edit' #- there are some errors
+			#redirect_to edit_book_path(@book)
 		end
 	end
 	
@@ -61,7 +64,7 @@ class BooksController < ApplicationController
 	private
 
 		def book_params
-			params.require(:book).permit(:title, :description, :author, :category_id, :book_img)
+			params.require(:book).permit(:title, :description, :author, :category_id, :book_img, :book_file)
 		end
 	
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,16 @@ class Book < ApplicationRecord
 	belongs_to :user
 	belongs_to :category
 	has_many :reviews
-	
+
 	has_attached_file :book_img, styles: { book_index: "300x300>", book_show: "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
 
+  has_attached_file :book_file
+	validates_attachment :book_file, :content_type => { :content_type => %w(application/epub application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
+
+  validates :title, length: { in: 4..100 }
+  validates :description, length: { in: 10..650 }
+  validates :author, presence: true
+
+#
 end
